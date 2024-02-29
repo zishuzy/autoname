986f49b9a542752da638b05ea26b3303960f7f48
zishu <zishuzy@gmail.com>
2024-02-29 19:40:00 +0800

    :sparkles: feat(main): init preject
    Signed-off-by: zishu <zishuzy@gmail.com>

---
+++ src/main.rs
@@ -0,0 +1,63 @@
+use clap::Parser;
+use std::fs;
+use std::path::PathBuf;
+
+/// Auto rename TV series files.
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// The path of the tv
+    #[arg(short, long)]
+    path: PathBuf,
+
+    /// The name of the tv
+    #[arg(short, long, value_name = "TV_NAME")]
+    name: Option<String>,
+
+    /// The suffix of file
+    #[arg(short = 'x', long)]
+    suffix: Option<String>,
+
+    /// The season of the tv
+    #[arg(short, long, default_value_t = 1)]
+    season: u8,
+
+    /// The type of sort
+    #[arg(short = 't', long, default_value_t = 1)]
+    sort: u8,
+}
+
+fn main() {
+    let args = Args::parse();
+
+    println!("args: {:#?}", args);
+
+    let dst_path = args.path.as_path();
+    if !dst_path.exists() {
+        eprintln!("The path does not exist, {}", dst_path.display());
+        return;
+    }
+
+    if let Ok(entries) = fs::read_dir(dst_path) {
+        for entry in entries {
+            if let Ok(entry) = entry {
+                let path = entry.path();
+
+                // 打印文件或子目录的路径
+                println!("{}", path.display());
+
+                // 如果是目录，可以递归遍历
+                if path.is_dir() {
+                    // 递归遍历子目录
+                    // traverse_directory(&path);
+                }
+            }
+        }
+    } else {
+        println!("Failed to read directory.");
+    }
+
+    if let Some(name) = args.name.as_deref() {
+        println!("Value for name: {:?}", name);
+    }
+}
