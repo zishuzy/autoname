68afd340e2de8e9b11f8f8790485a11cc35c439a
zishu <zishuzy@gmail.com>
2024-03-06 16:48:53 +0800

    :sparkles: feat(main): Code optimization
    Signed-off-by: zishu <zishuzy@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use clap::Parser;
-use std::{collections::HashMap, fs, path::PathBuf};
+use std::{collections::HashMap, fs, path};
 
 /// Auto rename TV series files.
 #[derive(Parser, Debug)]
@@ -7,15 +7,15 @@ use std::{collections::HashMap, fs, path::PathBuf};
 struct Args {
     /// The path of the tv file.
     #[arg(short, long)]
-    path: PathBuf,
+    path: path::PathBuf,
 
     /// Tv name
     #[arg(short, long, value_name = "TV_NAME")]
     name: String,
 
     /// The file extension to be renamed, if not set, the most used extension will be used.
-    #[arg(short = 'x', long)]
-    suffix: Option<String>,
+    #[arg(short, long)]
+    ext: Option<String>,
 
     /// The season of the tv
     #[arg(short, long, default_value_t = 1)]
@@ -24,31 +24,64 @@ struct Args {
     /// The type of sort. 1: ascending, 2: descending
     #[arg(short = 't', long, default_value_t = 1)]
     sort: u8,
+
+    /// Whether to rename files. 0: no, 1: yes
+    #[arg(short, long, default_value_t = 0)]
+    rename: u8,
+}
+
+fn read_dir(dst_path: &path::Path) -> Vec<fs::DirEntry> {
+    match fs::read_dir(dst_path) {
+        Ok(entries) => entries.filter_map(Result::ok).collect(),
+        Err(_) => {
+            eprintln!("Failed to read directory!");
+            Vec::new()
+        }
+    }
+}
+
+fn detect_ext(entries: &[fs::DirEntry]) -> Option<String> {
+    let mut ext_count: HashMap<String, usize> = HashMap::new();
+
+    for entry in entries.iter().filter(|e| !e.path().is_dir()) {
+        if let Some(ext) = entry.path().extension() {
+            let count = ext_count
+                .entry(ext.to_string_lossy().into_owned())
+                .or_insert(0);
+            *count += 1;
+        }
+    }
+
+    ext_count
+        .into_iter()
+        .max_by_key(|&(_, count)| count)
+        .map(|(ext, _)| ext)
 }
 
 fn main() {
     let mut args = Args::parse();
-    let mut vec_entries: Vec<fs::DirEntry> = Vec::new();
-
-    println!("args: {:#?}", args);
-
     let dst_path = args.path.as_path();
+
     if !dst_path.exists() {
-        eprintln!("The path does not exist, {}", dst_path.display());
+        eprintln!("The path does not exist, path[{}]", dst_path.display());
         return;
     }
 
-    if let Ok(entries) = fs::read_dir(dst_path) {
-        for entry in entries {
-            if let Ok(entry) = entry {
-                vec_entries.push(entry);
-            }
+    // 读取目录中的文件到 vec 中
+    let mut vec_entries = read_dir(dst_path);
+
+    if args.ext.is_none() {
+        println!("No specified file extension, than detect the most used extension.");
+        args.ext = detect_ext(&vec_entries);
+        if let Some(ref ext) = args.ext {
+            println!("Got the most used extension: {:?}", ext);
         }
-    } else {
-        println!("Failed to read directory.");
     }
 
-    // 排序 vec_entries
+    // 移除不需要执行重命名的文件
+    vec_entries.retain(|e| matches!(e.path().extension(), Some(ext) if ext == args.ext.as_deref().unwrap_or_default()));
+
+    // 对待重命名的文件进行排序
     vec_entries.sort_by(|a, b| {
         if args.sort == 1 {
             a.path().cmp(&b.path())
@@ -57,64 +90,38 @@ fn main() {
         }
     });
 
-    if args.suffix == None {
-        let mut ext_count: HashMap<String, usize> = HashMap::new();
-        let mut max_count = 0;
-        let mut max_ext = String::new();
-        println!("No set suffix, than detect the most used extension.");
-        for entry in vec_entries.iter() {
-            let path = entry.path();
-            // println!("{}", path.display());
-            if path.is_dir() {
+    // 执行重命名逻辑
+    for (count, entry) in vec_entries
+        .iter()
+        .enumerate()
+        .filter(|(_, e)| !e.path().is_dir())
+    {
+        if let Some(ext) = entry.path().extension() {
+            if ext.to_string_lossy() != args.ext.as_deref().unwrap_or_default() {
                 continue;
             }
-            if let Some(ext) = path.extension() {
-                let count = ext_count
-                    .entry(ext.to_str().unwrap().to_string())
-                    .or_insert(0);
-                *count += 1;
-                if *count > max_count {
-                    max_count = *count;
-                    max_ext = ext.to_str().unwrap().to_string();
-                }
-            }
-        }
-        println!("Got the most used extension: {:?}", max_ext);
-        args.suffix = Some(max_ext);
-    }
 
-    println!("Value for name: {:?}", args.name);
-
-    let mut count = 1;
-    for entry in vec_entries {
-        let path = entry.path();
-        if path.is_dir() {
-            continue;
-        }
-        if let Some(ext) = path.extension() {
-            if ext.to_str().unwrap() != args.suffix.as_deref().unwrap() {
-                continue;
-            }
             let name = format!(
                 "{}.S{:02}E{:02}.{}",
                 args.name,
                 args.season,
-                count,
-                args.suffix.as_deref().unwrap()
+                count + 1,
+                args.ext.as_deref().unwrap_or_default()
             );
-            count += 1;
-            match fs::rename(
-                &path,
-                PathBuf::from(format!("{}/{}", dst_path.display(), name)),
-            ) {
-                Ok(_) => println!(
-                    "Renamed: {} => {}",
-                    // path.file_name().unwrap().to_str().unwrap().display(),
-                    path.file_name().unwrap().to_str().unwrap(),
-                    name
-                ),
-                Err(e) => println!("Failed to rename: {}", e),
+            if args.rename == 0 {
+                println!("{} => {}", entry.file_name().to_string_lossy(), name);
+            } else {
+                match fs::rename(&entry.path(), dst_path.join(&name)) {
+                    Ok(_) => println!(
+                        "Renamed: {} => {}",
+                        entry.file_name().to_string_lossy(),
+                        name
+                    ),
+                    Err(e) => eprintln!("Failed to rename: {}", e),
+                }
             }
+        } else {
+            eprintln!("Failed to get extension: {}", entry.path().display());
         }
     }
 }
